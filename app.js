590d2da567d2f45b7d634b1e532a793aba1d32cf
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-06-23 15:24:36 +0500

    add

--- app.js
+++ app.js
@@ -5,9 +5,38 @@ let project = document.getElementById('project');
 let contact = document.getElementById('contact');
 let check = document.getElementById('check')
 
-function myHome(){
+function myHome() {
    check.checked = false;
 }
-home.addEventListener('click',myHome)
-project.addEventListener('click',myHome)
-contact.addEventListener('click',myHome)
+home.addEventListener('click', myHome)
+project.addEventListener('click', myHome)
+contact.addEventListener('click', myHome)
+
+function activeHome() {
+   home.style.backgroundColor = 'goldenrod';
+   home.style.transition = '0.2s';
+   project.style.backgroundColor = 'none';
+   contact.style.backgroundColor = 'none'
+
+}
+
+window.onload = activeHome()
+function activeProject() {
+   project.style.backgroundColor = 'goldenrod'
+   project.style.transition = '0.2s';
+   home.style.backgroundColor = 'transparent';
+   contact.style.backgroundColor = 'transparent'
+
+}
+
+project.addEventListener('click', activeProject)
+
+function activeContact() {
+   contact.style.backgroundColor = 'goldenrod'
+   contact.style.transition = '0.2s';
+   home.style.backgroundColor = 'transparent';
+   project.style.backgroundColor = 'transparent'
+
+}
+
+contact.addEventListener('click', activeContact)
